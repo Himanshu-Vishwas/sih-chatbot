efdaf6a892dab7422083f2ab3d94d3a6da0d0e82
prameet <kumarprameet18@gmail.com>
2023-09-27 23:58:00 +0530

    status tracking page created and finalied link to logo added

---
+++ screens/status/check_js.js
@@ -0,0 +1,25 @@
+function trackPNR() {
+    const pnrNumber = document.getElementById("pnrInput").value;
+
+    if (pnrNumber == "REF1695836486453") { // Note: Compare with a string
+        // Show a success message using SweetAlert
+        Swal.fire({
+            icon: 'success',
+            title: 'Complaint Accepted',
+            text: `PNR Number: ${pnrNumber}`,
+        });
+
+        // Update the status display
+        document.getElementById("statusDisplay").textContent = "Complaint accepted";
+    } else {
+        // Show an error message using SweetAlert
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Invalid Reference number or not found.',
+        });
+
+        // Update the status display
+        document.getElementById("statusDisplay").textContent = "Invalid Reference number or not found.";
+    }
+}
